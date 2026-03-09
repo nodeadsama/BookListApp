d1e2c92479cb751960eedd7b48b95dbdc1f04146
nodeadsama <47506666+nodeadsama@users.noreply.github.com>
2026-03-09 23:42:12 +0100

    Added login func

--- BeServer/src/controllers/UserController.php
+++ BeServer/src/controllers/UserController.php
@@ -3,5 +3,17 @@
 require_once __DIR__ . '/../services/UserService.php';
 
 class UserController {
+    public function login($request, $response, $args) {
+        $parsedBody = $request->getParsedBody(); // JSON expected (user details)
+
+        $username = $parsedBody['username'] ?? '';
+        $password = $parsedBody['password'] ?? '';
+
+        $service = new UserService();
+        $result = $service->login($username, $password);
+
+        $response->getBody()->write(json_encode($result));
+        return $response->withHeader('Content-Type', 'application/json');
+    }
 
 }
