4802dec5a65936157eaa447e0d585a64ce4d65a4
nodeadsama <47506666+nodeadsama@users.noreply.github.com>
2026-03-09 23:43:49 +0100

    Added postUser(login) call

--- BeServer/public/index.php
+++ BeServer/public/index.php
@@ -15,8 +15,11 @@ $app->get('/hello', function ($request, $response, $args) {
 });
 
 require_once __DIR__ . '/../src/Controllers/BookController.php';
+require_once __DIR__ . '/../src/Controllers/UserController.php';
 $bookController = new BookController();
+$userController = new UserController();
 
+// Books calls
 $app->get('/books', [$bookController, 'getBooks']);
 $app->get('/books/{id}', [$bookController, 'getBook']);
 
@@ -27,5 +30,7 @@ $app->put('/books/{id}', [$bookController, 'updateBook']);
 
 $app->delete('/books/{id}', [$bookController, 'deleteBook']);
 
+// Users calls
+$app->post('/auth/login', [$userController, 'login']);
 
 $app->run();
